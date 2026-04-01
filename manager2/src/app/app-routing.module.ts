1e4edd4a44656c09b39b2d870cfa0b87f1730887
mboudet <mateo.boudet@gmail.com>
2026-04-01 15:13:40 +0000

    angular12

--- manager2/src/app/app-routing.module.ts
+++ manager2/src/app/app-routing.module.ts
@@ -159,7 +159,8 @@ const routes: Routes = [
     imports: [
         RouterModule.forRoot(
             routes,
-            { enableTracing: false } // <-- debugging purposes only
+            { enableTracing: false, relativeLinkResolution: 'legacy' } // <-- debugging purposes only
+ // <-- debugging purposes only
         )
     ],
     exports: [RouterModule],
